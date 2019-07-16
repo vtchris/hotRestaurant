9de41d589ee488c24f5965b66c58e94a9cb66a92
VTC <vtchris@hotmail.com>
2019-07-15 21:21:58 -0400

    update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require("path");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 var reservations = [];
 var waitlist = [];
